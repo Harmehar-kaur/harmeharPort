bf063d0c257be2f303371983999e67c5852cd798
Harmehar-kaur <kaurharmehar9717@gmail.com>
2024-05-03 18:26:02 +0530

    coursework page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -24,3 +24,67 @@ module.exports.courses = function(req,res){
     }
     
 }
+
+module.exports.journey = function(req,res){
+    try{
+        return res.render('courses', {
+            title: "Coursework"
+        });
+
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+    
+}
+
+module.exports.blogs = function(req,res){
+    try{
+        return res.render('courses', {
+            title: "Coursework"
+        });
+
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+    
+}
+
+module.exports.poetry = function(req,res){
+    try{
+        return res.render('courses', {
+            title: "Coursework"
+        });
+
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+    
+}
+
+module.exports.contact = function(req,res){
+    try{
+        return res.render('courses', {
+            title: "Coursework"
+        });
+
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+    
+}
+
+module.exports.login=function(req,res){
+    try{
+        ('courses', {
+            title: "Coursework"
+        });
+
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+}
